0448f9e65e9638ca8cb6a58c8a8cb49130ac4540
Mostafa Ahmed <devil3ryan48@gmail.com>
2022-08-26 20:50:49 +0200

    Initial express setup

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const dotenv = require('dotenv');
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5555;
 
